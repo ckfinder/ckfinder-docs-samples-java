d02b6465d42b97d75374b1623d42d43f10227a37
Pawel Wiaderny <pawel.wiaderny@gmail.com>
2019-06-14 15:12:41 +0200

    Add warning in AlwaysTrueAuthenticator

--- src/main/java/example/authentication/AlwaysTrueAuthenticator.java
+++ src/main/java/example/authentication/AlwaysTrueAuthenticator.java
@@ -6,6 +6,12 @@ import javax.inject.Inject;
 import javax.inject.Named;
 import javax.servlet.http.HttpServletRequest;
 
+/**
+ * WARNING: Your authenticator should never simply return true. By doing so,
+ * you are allowing "anyone" to upload and list the files on your server.
+ * You should implement some kind of session validation mechanism to make
+ * sure that only trusted users can upload or delete your files.
+ */
 @Named
 public class AlwaysTrueAuthenticator implements Authenticator {
     @Inject
